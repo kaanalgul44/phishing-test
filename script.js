fd2481af0c41680024c3bc3ff2a6bd7e159cf316
Kaan Algül <kaanalgul044@gmail.com>
2025-08-30 17:13:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+async function checkEmail() {
+    const emailText = document.getElementById('emailText').value;
+    const resultDiv = document.getElementById('result');
+
+    const response = await fetch('https://phishing-backend.onrender.com/predict', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ email_text: emailText })
+    });
+
+    const data = await response.json();
+    resultDiv.textContent = "Tahmin: " + (data.prediction || "Bilinmiyor").toUpperCase();
+}
